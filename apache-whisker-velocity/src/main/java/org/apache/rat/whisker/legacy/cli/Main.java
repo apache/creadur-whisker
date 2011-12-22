40ed780d23f178cad855f1f18a741ffcddae952c
Robert Burrell Donkin <rdonkin@apache.org>
2011-12-22 21:13:12 +0000

    Start working on factoring out velocity engine module
    git-svn-id: https://svn.apache.org/repos/asf/incubator/rat/whisker/trunk@1222457 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apache-whisker-velocity/src/main/java/org/apache/rat/whisker/legacy/cli/Main.java
@@ -0,0 +1,126 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+package org.apache.rat.whisker.legacy.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.rat.whisker.legacy.app.Act;
+import org.apache.rat.whisker.legacy.app.Whisker;
+import org.apache.rat.whisker.legacy.app.WhiskerVelocity;
+
+/**
+ * Command line interface for whisker.
+ */
+public class Main {
+
+    /**
+     * 
+     */
+    private static final String APP_NAME = "apache-rat-whisker";
+    
+    /**
+     * Returns okay to system.
+     */
+    private static final int SYSTEM_EXIT_OK = 0;
+    private static final int SYSTEM_EXIT_CLI_PARSE_FAILED = 1;
+
+    /**
+     * Bootstraps application.
+     * @param args 
+     */
+    public static void main(String[] args) throws Exception {
+        System.exit(new Main(app()).run(args));
+    }
+    
+    private static Whisker app() {
+        return new WhiskerVelocity();
+    }
+    
+    private final Whisker whisker;
+
+    public Main(Whisker whisker) {
+        super();
+        this.whisker = whisker;
+    }
+
+    private CommandLineParser parser() {
+        return new GnuParser();
+    }
+    
+    public CommandLine parse(final String[] args) throws ParseException {
+        return parser().parse(options(), args);
+    }
+
+    public Whisker configure(final String[] args) throws ParseException {
+        return configure(parse(args));
+    }
+    
+    /**
+     * Configures the application from the command line given.
+     * @param parse commandLine not null
+     * @return not null
+     * @throws MissingOptionException 
+     */
+    private Whisker configure(final CommandLine commandLine) throws MissingOptionException {
+        whisker.setSource(CommandLineOption.SOURCE.getOptionValue(commandLine));
+        whisker.setLicenseDescriptor(CommandLineOption.LICENSE_DESCRIPTION.getOptionValue(commandLine));
+        if (CommandLineOption.ACT_TO_AUDIT.isSetOn(commandLine)) {
+            whisker.setAct(Act.AUDIT);
+        } else if (CommandLineOption.ACT_TO_GENERATE.isSetOn(commandLine)) {
+            whisker.setAct(Act.GENERATE);
+        }
+        
+        if (whisker.getSource() == null && whisker.getAct().isSourceRequired()) {
+            throw new MissingOptionException("-" + CommandLineOption.SOURCE.getShortName() + " " + CommandLineOption.SOURCE.getDescription());
+        }
+        return whisker;
+    }
+
+    public int run(final String[] args) throws Exception {
+        try {
+            configure(args).act();
+            return SYSTEM_EXIT_OK;
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            help();
+            return SYSTEM_EXIT_CLI_PARSE_FAILED;
+        }
+    }
+
+    /**
+     * Prints out help.
+     */
+    private void help() {
+        final HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(APP_NAME, options());
+    }
+
+    /**
+     * Builds options for the command line.
+     * @return not null
+     */
+    private Options options() {
+        return CommandLineOption.options();
+    }
+}
---
+++ apache-whisker-velocity/src/main/java/org/apache/rat/whisker/legacy/scan/FromFileSystem.java
@@ -0,0 +1,291 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License. 
+ */
+package org.apache.rat.whisker.legacy.scan;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+
+/**
+ * 
+ */
+public class FromFileSystem {
+    
+    /**
+     * @param base
+     */
+    public FromFileSystem() {
+        super();
+    }
+
+    public Collection<Directory> withBase(final String base) throws IOException {
+        return new Builder(base).build();
+    }
+    
+    private static class Builder {
+        /**
+         * 
+         */
+        private static final int DEFAULT_INITIAL_CAPACITY = 64;
+        
+        private final File base;
+        private final Set<Directory> directories;
+        private final Queue<Work> workInProgress;
+        private final Collection<Work> workDone;
+        
+        /**
+         * @param base
+         */
+        public Builder(String base) {
+            this(base, DEFAULT_INITIAL_CAPACITY);
+        }
+
+        /**
+         * @param base
+         */
+        public Builder(String base, int initialCapacity) {
+            super();
+            this.base = new File(base);
+            directories = new TreeSet<Directory>();
+            workInProgress = new LinkedList<Work>();
+            workDone = new ArrayList<Work>(initialCapacity);
+        }
+        
+        public Collection<Directory> build() throws IOException {
+            put(base).andWork().untilDone();
+            return directories;
+        }
+
+        /**
+         * 
+         */
+        private void untilDone() {
+            // TODO: wait until all threads done
+        }
+
+        private Builder put(File file) {
+            return put(new Work(file));
+        }
+        
+        /**
+         * @param base
+         */
+        private Builder put(Work work) {
+            if (work != null) {
+                if (workDone.contains(work)) {
+                    alreadyDone(work);
+                } else {
+                    this.workInProgress.add(work);
+                }
+            }
+            return this;
+        }
+        
+        /**
+         * @param work
+         */
+        private void alreadyDone(Work work) {
+            System.out.println("Already done " + work);
+        }
+
+        private Builder andWork() {
+            while (!workInProgress.isEmpty()) {
+                workDone.add(workOn(workInProgress.poll()));
+            }
+            return this;
+        }
+
+        /**
+         * @param poll
+         * @return
+         */
+        private Work workOn(final Work next) {
+            for (final String name: next.contents()) {
+                put(next.whenDirectory(name));
+            }
+            directories.add(next.build());
+            return next;
+        }
+        
+        
+        private static class Work {
+            
+            private static final String BASE_DIRECTORY = ".";
+            private final String name;
+            private final File file;
+            
+            /**
+             * @param file
+             */
+            public Work(File file) {
+                this(BASE_DIRECTORY, file);
+            }
+
+            /**
+             * @param name
+             * @param file
+             */
+            public Work(String name, File file) {
+                if (! file.exists()) {
+                    throw new IllegalArgumentException("Expected '"+ file.getAbsolutePath() + "' to exist");
+                }
+                if (! file.isDirectory()) {
+                    throw new IllegalArgumentException("Expected '"+ file.getAbsolutePath() + "' to be a directory");
+                }
+                this.name = name;
+                this.file = file;
+            }
+            
+            public String[] contents() {
+                final String[] contents = file.list();
+                if (contents == null) {
+                    throw new IllegalArgumentException("Cannot list content of " + file);
+                }
+                return contents;
+            }
+
+            /**
+             * @return
+             */
+            public Directory build() {
+                final Directory result = new Directory().setName(name);
+                for (final String name: contents()) {
+                    if (isResource(name)) {
+                        result.addResource(name);
+                    }
+                }
+                return result;
+            }
+
+            /**
+             * @param name
+             * @return
+             */
+            private boolean isResource(final String name) {
+                return !isDirectory(name);
+            }
+
+            /**
+             * @param name
+             * @return
+             */
+            private boolean isDirectory(final String name) {
+                return file(name).isDirectory();
+            }
+            
+            /**
+             * @param name
+             * @return
+             */
+            public Work whenDirectory(String name) {
+                final File file = file(name);
+                final Work result;
+                if (file.isDirectory()) {
+                    result = new Work(path(name), file);
+                } else {
+                    result = null;
+                }
+                return result;
+            }
+
+            /**
+             * @param name
+             * @return
+             */
+            private String path(String name) {
+                final String result;
+                if (isBaseDirectory()) {
+                    result = name;
+                } else {
+                    result = this.name + "/" + name;
+                }
+                return result;
+            }
+
+            /**
+             * @return
+             */
+            private boolean isBaseDirectory() {
+                return BASE_DIRECTORY.equals(this.name);
+            }
+
+            /**
+             * @param name
+             * @return
+             */
+            private File file(String name) {
+                return new File(this.file, name);
+            }
+
+            /**
+             * @see java.lang.Object#hashCode()
+             */
+            @Override
+            public int hashCode() {
+                final int prime = 31;
+                int result = 1;
+                result = prime * result
+                        + ((file == null) ? 0 : file.hashCode());
+                result = prime * result
+                        + ((name == null) ? 0 : name.hashCode());
+                return result;
+            }
+            
+            /**
+             * @see java.lang.Object#equals(java.lang.Object)
+             */
+            @Override
+            public boolean equals(Object obj) {
+                if (this == obj)
+                    return true;
+                if (obj == null)
+                    return false;
+                if (getClass() != obj.getClass())
+                    return false;
+                Work other = (Work) obj;
+                if (file == null) {
+                    if (other.file != null)
+                        return false;
+                } else if (!file.equals(other.file))
+                    return false;
+                if (name == null) {
+                    if (other.name != null)
+                        return false;
+                } else if (!name.equals(other.name))
+                    return false;
+                return true;
+            }
+            
+            /**
+             * @see java.lang.Object#toString()
+             */
+            @Override
+            public String toString() {
+                return "Work [name=" + name + ", file=" + file + "]";
+            }
+        }
+    }
+}
