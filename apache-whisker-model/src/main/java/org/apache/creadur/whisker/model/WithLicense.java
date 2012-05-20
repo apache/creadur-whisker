7c74da1368494634e292e42ad67b3db0e3e857cc
Robert Burrell Donkin <rdonkin@apache.org>
2012-05-20 15:51:33 +0000

    Fixed some checkstyle violations
    git-svn-id: https://svn.apache.org/repos/asf/creadur/whisker/trunk@1340756 13f79535-47bb-0310-9956-ffa450edef68

--- apache-whisker-model/src/main/java/org/apache/creadur/whisker/model/WithLicense.java
+++ apache-whisker-model/src/main/java/org/apache/creadur/whisker/model/WithLicense.java
@@ -23,18 +23,29 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * 
+ * Groups resources sharing a license and claimed copyright.
  */
 public class WithLicense implements Comparable<WithLicense> {
 
+    /** License shared by contained resources, not null. */
     private final License license;
+    /** Resources grouped by responsible organisation. */
     private final Collection<ByOrganisation> organisations;
+    /** Copyright claim shared by contained resources. */
     private final String copyrightNotice;
+    /** Parameters to specialise the license family template. */
     private final Map<String, String> parameters;
 
     /**
-     * @param license
-     * @param element
+     * Groups resources sharing a license and copyright claim.
+     * @param license License shared by contained resources,
+     * not null
+     * @param copyrightNotice copyright claim
+     * shared by contained resources, not null
+     * @param parameters parameters to specialise
+     * the license family template, not null
+     * @param organisations resources grouped by
+     * responsible organisation, not null
      */
     public WithLicense(final License license, final String copyrightNotice,
             final Map<String, String> parameters,
@@ -46,43 +57,84 @@ public class WithLicense implements Comparable<WithLicense> {
         this.organisations = Collections.unmodifiableCollection(organisations);
     }
 
+    /**
+     * Gets the copyright claim shared
+     * by the resources contained.
+     * @return not null
+     */
     public String getCopyrightNotice() {
         return this.copyrightNotice;
     }
 
+    /**
+     * Gets the presentation name for the license
+     * shared by the resources contained.
+     * @return not null
+     */
     public String getName() {
         return this.license.getName();
     }
 
+    /**
+     * Gets a locator for the license
+     * shared by the resources contained.
+     * @return not null
+     */
     public String getURL() {
         return this.license.getURL();
     }
 
+    /**
+     * Gets license meta-data shared by the resources
+     * contained.
+     * @return not null
+     */
     public License getLicense() {
         return this.license;
     }
 
+    /**
+     * Gets the license legalise shared by the resources
+     * contained. Computed by applying the parameters
+     * to the license template.
+     * @return not null
+     * @throws LicenseTemplateException when the license
+     * text cannot be generated from the template
+     */
     public String getText() throws LicenseTemplateException {
         return this.license.getText(this.parameters);
     }
 
+    /**
+     * Gets resources grouped by responsible organisation.
+     * @return not null
+     */
     public Collection<ByOrganisation> getOrganisations() {
         return this.organisations;
     }
 
+    /**
+     * Gets the parameters substituted into the license
+     * template when generating the license legalise.
+     * @return not null
+     */
     public Map<String, String> getParameters() {
         return this.parameters;
     }
 
     /**
+     * Based on license.
      * @see java.lang.Comparable#compareTo(java.lang.Object)
+     * @param other possibly null
+     * @return license comparison
      */
     public int compareTo(final WithLicense other) {
         return this.license.compareTo(other.getLicense());
     }
 
     /**
-     * @param visitor
+     * Accepts a visit.
+     * @param visitor possibly null
      */
     public void accept(final Visitor visitor) {
         if (visitor != null && visitor.traverseWithLicense()) {
@@ -94,10 +146,13 @@ public class WithLicense implements Comparable<WithLicense> {
     }
 
     /**
-     * @return
+     * Should information about the source distribution of
+     * contained resources be included?
+     * @return true when license asks that information
+     * about the source distribution is included,
+     * false otherwise
      */
     public boolean isSourceRequired() {
         return this.license.isSourceRequired();
     }
-
 }
