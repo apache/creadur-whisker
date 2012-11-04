d183d39937118bf222d78d8fa103bf6eb6c800f3
Robert Burrell Donkin <rdonkin@apache.org>
2012-11-04 13:26:23 +0000

    WHISKER-10 Fixtures for improved integration testing
    git-svn-id: https://svn.apache.org/repos/asf/creadur/whisker/trunk@1405541 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apache-whisker-plugin4maven/src/test/java/org/apache/creadur/whisker/it/Helpers.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.creadur.whisker.it;
+
+import static org.apache.creadur.whisker.it.Not.not;
+
+import java.io.File;
+
+public class Helpers {
+
+    public static AnyLineContainsCheck aLineContainsCDDL1() {
+        return new AnyLineContainsCheck("COMMON DEVELOPMENT AND DISTRIBUTION LICENSE (CDDL) Version 1.0");
+    }
+
+    public static AnyLineContainsCheck aLineContains(String value) {
+        return new AnyLineContainsCheck(value);
+    }
+
+    private static final String PATH_TO_LICENSE_FILE = "target/LICENSE";
+
+    public static boolean noticeIsMissing(File basedir) {
+        return fileIsMissing(basedir, "target/NOTICE");
+    }
+
+    private static boolean fileIsMissing(File basedir, final String string) {
+        return not(new File(basedir, string).exists());
+    }
+
+    public static boolean licenseIsMissing(File basedir) {
+        return fileIsMissing(basedir, PATH_TO_LICENSE_FILE);
+    }
+
+    public static LicenseVerifier licenseIn(File basedir) {
+        return new LicenseVerifier(new File(basedir, PATH_TO_LICENSE_FILE));
+    }
+
+    public static Results results() {
+        return new Results();
+    }
+}
---
+++ apache-whisker-plugin4maven/src/test/java/org/apache/creadur/whisker/it/Results.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.creadur.whisker.it;
+
+public class Results {
+
+    private final StringBuilder builder = new StringBuilder("***FAILED***\n");
+    private boolean failed = false;
+
+    public boolean hasFailed() {
+        return failed;
+    }
+
+    public Results fail(String message) {
+        builder.append("  ");
+        builder.append(message);
+        builder.append("\n");
+        failed = true;
+        return this;
+    }
+
+    public String report() {
+        final String result;
+        if (failed) {
+            endLine();
+            endLine();
+            endLine();
+            result = builder.toString();
+        } else {
+            result = null;
+        }
+        return result;
+    }
+
+    private void endLine() {
+        builder.append('\n');
+    }
+
+    public Results titled(final String title) {
+        endLine();
+        endLine();
+        endLine();
+        builder.append('#').append('#').append(' ').append(title);
+        endLine();
+        endLine();
+        return this;
+    }
+}
